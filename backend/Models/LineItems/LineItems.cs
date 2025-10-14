e4c5a01996df307983b8557a1f89667c9b1ee818
Lolillya <someonesusernam@gmail.com>
2025-10-14 23:25:09 +0800

    added line-item table model

---
+++ backend/Models/LineItems/LineItems.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models.Inventory;
+
+namespace backend.Models.LineItems
+{
+    public class LineItems
+    {
+        [Key]
+        public int LineItem_ID { get; set; }
+
+        // FKs
+        public int Product_ID { get; set; }
+        [ForeignKey(nameof(Product_ID))]
+        public Product Product { get; set; } = null!;
+
+        public string Unit { get; set; } = null!;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SubTotal { get; set; }
+        public int Quantity { get; set; }
+    }
+}
